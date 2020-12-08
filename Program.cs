4ca6bd2d08c9ef25964bfc23ae058b21c9b56eed
Mario Hofstätter <mario.hofstaetter@automationx.com>
2020-12-08 20:04:12 +0100

    xx

--- Program.cs
+++ Program.cs
@@ -15,7 +15,10 @@ namespace ConsoleApp1
             var agentHost = Environment.GetEnvironmentVariable("JAEGER_AGENTHOST") ?? "localhost";
             var agentPort = int.Parse(Environment.GetEnvironmentVariable("JAEGER_AGENTPORT")?? "6831");
 
+            var loopCount = int.Parse(Environment.GetEnvironmentVariable("DEMOCLIENT_LOOPCOUNT")?? "500");
+
             System.Console.WriteLine($"Using agenthost '{agentHost}:{agentPort}'");
+            System.Console.WriteLine($"Running  '{loopCount}' loops");
 
             using var tracerProvider = Sdk.CreateTracerProviderBuilder()
                 .AddSource(ActivitySource.Name)
@@ -28,23 +31,28 @@ namespace ConsoleApp1
 
             Console.WriteLine("Starting loop");
 
-            for (var i = 0; i < 50000; i++)
+            for (var i = 0; i < loopCount; i++)
             {
-                using var activity = ActivitySource.StartActivity(i.ToString());
-                activity.AddEvent(new ActivityEvent("Event"));
-                activity.AddTag("foo", "bar");
-
-                using var childActivity = ActivitySource.StartActivity($"{i} child", ActivityKind.Internal);
-                childActivity.AddEvent(new ActivityEvent("Child Event"));
-                childActivity.AddTag("bar", "baz");
-
-                if(i % 50 == 0)
+                using(var activity = ActivitySource.StartActivity(i.ToString()))
                 {
-                    System.Threading.Thread.Sleep(50);
+                    activity.AddEvent(new ActivityEvent("Event"));
+                    activity.AddTag("foo", "bar");
+
+                    using(var childActivity = ActivitySource.StartActivity($"{i} child", ActivityKind.Internal))
+                    {
+                        childActivity.AddEvent(new ActivityEvent("Child Event"));
+                        childActivity.AddTag("bar", "baz");
+
+                        if(i % 50 == 0)
+                        {
+                            System.Threading.Thread.Sleep(50);
+                        }
+                    }
                 }
             }
 
-            Console.WriteLine("Loop done, Exit");
+            Console.WriteLine("Loop done, wait 5 seconds because it seems not everything gets flushed");
+            System.Threading.Thread.Sleep(5000);
         }
     }
 }
